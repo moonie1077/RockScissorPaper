40c585366e7f68269fe0e7e6fe84fd31ea8be3d7
g145773 <andrew.j.laguana@kp.org>
2023-03-03 15:33:14 -0800

    Transition to UI

--- script.js
+++ script.js
@@ -23,59 +23,90 @@ function formatPlayerInput (input) {
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
+      divRound.textContent = "It was a tie!  No one wins.";
       return 0; //representing a Tie
     } else if (playerSelection === "Rock" && computerSelection === "Scissors" ||
                 playerSelection === "Scissors" && computerSelection === "Paper" ||
                 playerSelection === "Paper" && computerSelection === "Rock")
-      {return 1;} //representing PlayerVictory}
-      else {return 2;} //representing ComputerVictory}
-
+      {
+         divRound.textContent = playerSelection + " beats " + computerSelection + ". Player wins!";
+         return 1;} //representing PlayerVictory}
+      else {
+         divRound.textContent = playerSelection + " does not beat " + computerSelection + ". Computer wins!";
+         return 2;} //representing ComputerVictory}
     }
 
-function game(){
-let scorePlayer = 0;
-let scoreComputer = 0;
-
-for (let i=1; i<6; i++){  //i starting at one to act as round counter in output
-    let formattedPlayerSelection = "default"
-    let compChoice = getComputerChoice();
-
-        while(true) {
-        //This while loop uses Regex to check for specific values of player input
-          playerPrompt = prompt("Please type Rock, Scissors, or Paper.");
-          if (playerPrompt.match (/^(rock|paper|scissors)$/i))
-             {
-             formattedPlayerSelection = formatPlayerInput(playerPrompt);
-             break;
-             }
-          else {alert ("Please enter a valid selection!")}
-        }
-        
-    switch (playRound(formattedPlayerSelection,compChoice)){
-        case 0:
-          scoreComputer++;
-          scorePlayer++;
-          console.log("It was a tie!  Point for each.");
+//function game(){
+//let scorePlayer = 0;
+//let scoreComputer = 0;
+//
+//for (let i=1; i<6; i++){  //i starting at one to act as round counter in output
+//    let formattedPlayerSelection = "default"
+//    let compChoice = getComputerChoice();
+//
+//       while(true) {
+//       //This while loop uses Regex to check for specific values of player input
+//         //playerPrompt = prompt("Please type Rock, Scissors, or Paper.");
+//         if (playerPrompt.match (/^(rock|paper|scissors)$/i))
+//            {
+//            formattedPlayerSelection = formatPlayerInput(playerPrompt);
+//            break;
+//            }
+//         else {alert ("Please enter a valid selection!")}
+//       }
+ function score (roundOutcome) {
+    switch (roundOutcome){
+        case 0:// removed tie condition for each point to simplify win
+         // scoreComputer++;
+         // scorePlayer++;
           break;
         case 1:
           scorePlayer++;
-          console.log (formattedPlayerSelection + " beats " + compChoice + ". Player wins!");
           break;
         case 2:
           scoreComputer++;
-          console.log (formattedPlayerSelection + " does not beat " + compChoice + ". Computer wins!");
           break;
         }
-    console.log("Round " + i + " results:  Computer: " + scoreComputer + "; Player: " + scorePlayer);
-    }
 
-    if (scoreComputer == scorePlayer)
-        {console.log ("Best of five was a tie!");}
-    else if (scoreComputer > scorePlayer)
-        {console.log ("Computer wins best of five!");}
-    else
-        {console.log ("Player wins best of five!");}
+ }
+ //   console.log("Round " + i + " results:  Computer: " + scoreComputer + "; Player: " + scorePlayer);
+ //   }
 
-    }
+function checkGameOver(adam, hal) {
+  if(adam == '5' || hal == '5') {
+    (adam == 5?
+            divHumanScore.textContent = "Human wins it all!"
+          : divCompScore.textContent = "Computer wins it all!"
+     )
+     playerButton.forEach((button) => {button.style.display = "none"});
+  }
+return;
+}
 
-game()
+ //   if (scoreComputer == scorePlayer)
+ //       {console.log ("Best of five was a tie!");}
+ //   else if (scoreComputer > scorePlayer)
+ //       {console.log ("Computer wins best of five!");}
+ //   else
+ //       {console.log ("Player wins best of five!");}
+ //
+ //   }
+
+//game()
+let scoreComputer = 0
+let scorePlayer = 0
+const playerButton = document.querySelectorAll('button');
+const divHumanScore = document.querySelector('#Player');
+const divCompScore = document.querySelector('#Computer');
+const divRound = document.querySelector('#Round');
+
+
+
+playerButton.forEach((button) => {
+    button.addEventListener('click',() => {
+        score((playRound(button.id, getComputerChoice())))
+        divHumanScore.textContent = "Human score is " + scorePlayer;     
+        divCompScore.textContent = "Computer score is " + scoreComputer;
+        checkGameOver(scorePlayer,scoreComputer) });
+    });
+
